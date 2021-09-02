6341993281d7c4f296efa270192f7d37ae8d29bb
Rey <85196372+inirey@users.noreply.github.com>
2021-09-02 18:58:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts = require('cfonts')
-CFonts.say('ERZA BY REY', {
+CFonts.say('ERZA', {
   colors: ['#f2aa4c'],
   font: 'block',
   align: 'center',
